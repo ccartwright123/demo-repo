d3fc5aee0c057e0d26136d39e18e8b0d82a894d4
ccartwright123 <86298594+ccartwright123@users.noreply.github.com>
2021-06-30 11:52:39 +0100

    Add files via upload

---
+++ src/calculator/cal.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class cal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MathFun.add(1, 2));
+		System.out.println(MathFun.sub(2, 1));
+		System.out.println(MathFun.mul(5, 3));
+		System.out.println(MathFun.div(15, 3));
+	}
+
+}
